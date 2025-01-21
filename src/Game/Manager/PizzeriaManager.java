ccdc9434c65613d5583828e727f7c2e72b95442a
RedWoodHD <Eliasganske@outlook.com>
2025-01-21 11:34:44 +0100

    Getter und Setter hinzufügen

--- src/Game/Manager/PizzeriaManager.java
+++ src/Game/Manager/PizzeriaManager.java
@@ -1,13 +1,17 @@
 package Game.Manager;
 
+import Game.Factor.EnemyName;
 import Game.Factor.RoomName;
+import Game.Object.Enemy;
 import Game.Object.Room;
 import java.util.Map;
 
 public class PizzeriaManager
 {
     RoomManager roomManager = new RoomManager();
+    EnemyManager enemyManager = new EnemyManager();
     Map<RoomName, Room> pizzeriaAllRooms = roomManager.allRooms;
+    Map<EnemyName, Enemy> pizzeriaAllEnemies = enemyManager.enemyMap;
 
     public RoomManager getRoomManager()
     {
@@ -28,6 +32,28 @@ public class PizzeriaManager
     public PizzeriaManager setPizzeriaAllRooms(Map<RoomName, Room> pizzeriaAllRooms)
     {
         this.pizzeriaAllRooms = pizzeriaAllRooms;
+        return this;
+    }
+
+    public EnemyManager getEnemyManager()
+    {
+        return enemyManager;
+    }
+
+    public PizzeriaManager setEnemyManager(EnemyManager enemyManager)
+    {
+        this.enemyManager = enemyManager;
+        return this;
+    }
+
+    public Map<EnemyName, Enemy> getPizzeriaAllEnemies()
+    {
+        return pizzeriaAllEnemies;
+    }
+
+    public PizzeriaManager setPizzeriaAllEnemies(Map<EnemyName, Enemy> pizzeriaAllEnemies)
+    {
+        this.pizzeriaAllEnemies = pizzeriaAllEnemies;
         return this;
     }
 }
