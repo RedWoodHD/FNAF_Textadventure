f0f5df36274be7285b3db07f0aff7270e3d5a9fc
RedWoodHD <Eliasganske@outlook.com>
2025-01-21 16:39:57 +0100

    Add Classes for the console Output

---
+++ src/Game/text_message/Colors.java
@@ -0,0 +1,14 @@
+package Game.text_message;
+
+public class Colors
+{
+    final static String red = "\u001b[31m";
+    final static String green = "\u001b[32m";
+    final static String yellow = "\u001b[33m";
+    final static String blue = "\u001b[34m";
+    final static String magenta = "\u001b[35m";
+    final static String cyan = "\u001b[36m";
+    final static String grey = "\u001b[37m";
+
+    final static String reset = "\u001b[0m";
+}
---
+++ src/Game/text_message/Map.java
@@ -0,0 +1,46 @@
+package Game.text_message;
+
+import static Game.text_message.Colors.magenta;
+import static Game.text_message.Colors.reset;
+
+/**Diese Klasse wird verwendet, um die Karte auf der Console auszugeben.
+ *Dazu wird folgende Methode genutzt: {@link #printMap()}
+ */
+public class Map
+{
+    /**Die Methode gibt die Karte auf der Console aus.
+     * Die Karte hat keinerlei Logik, sie ist rein optisch und zum verständnis des Spielers.
+     */
+    private static void printMap()
+    {
+        System.out.print(magenta);
+        System.out.println("""
+                                  ┌──────┐
+                                  │CAM 1A├─────────────┐
+                                  └─┬────┘             │
+                                    │                  │
+                   ┌─────┐ ┌────┬───┴──┬───────────────┴─────────┐
+                   │     ├─┤    │CAM 1B│                         │ ┌───┐
+                   │     ├─┤    └──────┘                         ├─┤ ┌─┴────┐
+                ┌──┴───┐ │ │                                     ├─┤ │CAM 7 ├┐
+                │CAM 5 │ │ │                                     │ │ └─┬─┬──┘│
+                └──┬───┘ │ │                                     │ │   ├─┤   │
+                   └─────┘┌┴─────┐                               │ │   │ └───┘
+                     ┌────┤CAM 1C│                               │ │   │ ┌───┐
+                     │    └┬─────┘                               │ │   ├─┤   │
+                     └─────┴───────┬┬─────────────┬┬────┬┬───────┘ └───┘ └───┘
+                                 ┌─┴┴─┐         ┌─┴┴─┐ ┌┴┴───────────┐
+                        ┌─────┐  │    │         │    │ │        ┌────┴─┐
+                     ┌──┴───┐ ├─┬┴────┴┐       ┌┴────┴┐│        │CAM 6 │
+                     │CAM 3 │ ├─┤CAM 2A│       │CAM 4A││        └────┬─┘
+                     └──┬───┘ │ ├──────┤       ├──────┤└─────────────┘
+                        │     │ ├──────┤┌─────┐├──────┤
+                        └─────┘ │CAM 2B├┤ YOU ├┤CAM 4B│
+                                └┬────┬┴┤  ▓  ├┴┬────┬┘
+                                 └────┘ └─────┘ └────┘
+                """);
+        System.out.print(reset);
+//        Folgende Zeichen wurden für die Map genutzt:
+// ┼,┬,┴,├,┤,═,║,┌,┐,└,┘,─,│,
+    }
+}
