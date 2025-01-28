b17f0014cddb00907fe12b776cc4610b73fa1bbd
RedWoodHD <Eliasganske@outlook.com>
2025-01-28 15:08:41 +0100

    Add Camera needed Classes

---
+++ src/Game/factor/CameraName.java
@@ -0,0 +1,16 @@
+package Game.factor;
+
+public enum CameraName
+{
+    CAM1A,
+    CAM1B,
+    CAM1C,
+    CAM2A,
+    CAM2B,
+    CAM3,
+    CAM4A,
+    CAM4B,
+    CAM5,
+    CAM6,
+    CAM7;
+}
--- src/Game/object/Camera.java
+++ src/Game/object/Camera.java
@@ -1,17 +1,29 @@
 package Game.object;
 
+import Game.factor.CameraName;
+
 import java.util.List;
 
 public class Camera
 {
     private List<Camera> connectedCameras;
+    private CameraName cameraName;
     private boolean isPlayerLookingAtIt;
+
+    public Camera(CameraName cameraName)
+    {
+        this.cameraName = cameraName;
+    }
 
     public void useCamera(){
         // Kann ich die überhaupt noch benutzen? Ist noch Strom da?
         // Geprinted was ich sehe.
         // Auswirkungen auf enemies (gesehen/nicht gesehen)
         // Energie wird vom Haus abgezogen.
+    }
+
+    public Enemy getWhatEnemyDoISee(){
+
     }
 
     public List<Camera> getConnectedCameras() {
