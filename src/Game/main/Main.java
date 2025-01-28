1079d1b5373cfa2cc014f00e5dfd6722bbc5cd16
RedWoodHD <Eliasganske@outlook.com>
2025-01-28 14:44:58 +0100

    Delete unused import

--- src/Game/main/Main.java
+++ src/Game/main/Main.java
@@ -1,9 +1,14 @@
 package Game.main;
 
+import Game.mechanic.FnafGame;
+
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
+
         FnafGame.runGame();
 //        System.out.println("1.Open Tablet\t 2.Exit Tablet\t 3.showCommands\t 4.Exit Game");
     }
