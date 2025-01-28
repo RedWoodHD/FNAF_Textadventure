d843ad24fe479d0efa4ee99578c79addfa969c66
RedWoodHD <Eliasganske@outlook.com>
2025-01-28 15:07:54 +0100

    Add some methods for the user input

--- src/Game/mechanic/FnafGame.java
+++ src/Game/mechanic/FnafGame.java
@@ -1,11 +1,10 @@
-package Game.main;
+package Game.mechanic;
 
 import Game.factor.EnemyName;
-import Game.factor.RoomName;
 import Game.manager.PizzeriaManager;
-import Game.mechanic.RoundMechanic;
-import Game.object.Camera;
-import Game.object.Room;
+import Game.text_message.GameInformation;
+
+import static Game.text_message.Map.printMap;
 
 public class FnafGame
 {
@@ -13,14 +12,17 @@ public class FnafGame
     {
         PizzeriaManager pizzeriaManager = new PizzeriaManager();
         RoundMechanic roundMechanic = new RoundMechanic();
+        InputMechanic inputMechanic = new InputMechanic();
         System.out.println(pizzeriaManager.getEnemyManager().getEnemyMap().get(EnemyName.FREDDY).getLevel());
-
+        printMap();
 
         for (int i = 1; i <= 20 ; i++) {
             roundMechanic.startRound(i,pizzeriaManager.getPizzeria());
 //            pizzeriaManager.getPizzeria().getRoomMap().get(RoomName.OFFICE).setCamera()
 
         }
+        GameInformation gameInformation = new GameInformation();
+        gameInformation.print6AM();
 
     }
 }
